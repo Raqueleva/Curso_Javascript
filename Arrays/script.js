0ab0a25e3c905363de3040217b360d643e046a7c
raqueleva <raquelevar@gmail.com>
2025-10-31 11:00:45 -0300

    readme updated

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,7 +1,23 @@
-// Arrays
+// Arrays (Listas)
 
-let cart = ['Agua', 'Arroz', 'Feijão', 'Carne']
-console.log(cart)
-console.log(`A minha lista de compras para hoje contém os itens: ${cart}`)
+// let cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
+// let numbers = [10, 20, 33, 40, 5, 14, true]
+// console.log(numbers)
+// console.log(numbers[3]) // Escolhe o numero que preciso (lembrando que a contagem se inicia sempre com 0)
+// console.log(numbers[0] + numbers[2])
+// cart[1] = 'Laranja' // Serve par alterar algum item da minha lista
+// console.log(cart)
+// console.log(`A minha lista de compras para hoje contém os itens: ${cart}`)
 // Para acessar itens dentro de uma array
-console.log(`A minha comida favorita é ${cart[1]} e ${cart[3]}`)
+// console.log(`A minha comida favorita é ${cart[1]} e ${cart[3]}`)
+
+////////////////////////////////////
+
+// Exercício:
+/*Criar uma solução que concatena o terceiro item da lista logo abaixo ao testo na <console></console>*/
+/*Iniciar com a segunite frase : 'In the second cage we have: e add o item Birds'*/
+
+let petshop = ['Dogs', 'Cats', 'Birds', 'Hamsters']
+console.log(`In the second cage we have: ${petshop[2]}`)
+
+//////////////////////////////////
